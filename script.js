1638535ef853c8e19c5f8c7a000783f143de1121
MarinaUtt <marinauteshova13@gmail.com>
2025-03-06 22:59:16 +0500

    refactor timer

--- script.js
+++ script.js
@@ -1,38 +1,74 @@
-const BASE_URL = 'https://ca0ca26afdb72df07d4d.free.beeceptor.com/api/';
+const BASE_URL = 'https://ca4b3cec7af0096f5da8.free.beeceptor.com/api/users/' ;
 const addForm = document.forms.addForm;
 const buttonForm = document.querySelector('.submit-task');
 const inputTask = document.querySelector('.input-task');
 const listTasks = document.querySelector('.tasks-list');
-const dayTimer= document.querySelector('day-input');
-const hourTimer= document.querySelector('hour-input');
-const minuteTimer= document.querySelector('minute-input');
+const dayTimer = document.querySelector('.day-input');
+const hourTimer = document.querySelector('.hour-input');
+const minuteTimer = document.querySelector('.minute-input');
 let taskId = '';
-let taskCompleted = '';
+const responsePromise = fetch(BASE_URL);
+
+const timerStart  = function(dayElem, hourElem, minuteElem, secondElem, day, hour, minute, second) { 
+  let minuteValue; 
+  let hourValue;
+  let secondValue;
+  return async function f() {
+    for (let j = day; j >= 0; j--) {
+      dayElem.textContent = j;
+      if (j < day){
+        hourValue = 23;
+      } else {
+        hourValue = hour;
+      }
+      for (let x = hourValue; x >= 0; x--) {
+        hourElem.textContent = x;
+        if (x == hour & j == day) {
+          minuteValue = minute;
+        } else minuteValue = 59;
+        for (let y = minuteValue; y >= 0; y--) {
+          minuteElem.textContent = y;
+          if (x == hour & j == day & y == minute) {
+            secondValue = second;
+          } else secondValue = 59;
+          for (let i = secondValue; i >= 0; i--) {
+            await new Promise(resolve => setTimeout(resolve,100));
+            secondElem.textContent = i; 
+          }
+        } 
+      }
+    }
+  } 
+}
 
 renderTasks();
 
 addForm.addEventListener ('submit', (event) => {
   event.preventDefault();
   if (!!taskId){
-    editTasks(taskId,inputTask.value,taskCompleted).then(() => renderTasks());
+    editTasks(taskId,inputTask.value).then(() => renderTasks());
     taskId = '';
-    taskCompleted = '';
     buttonForm.textContent = "Добавить";
   }
   else{
-    createTasks(inputTask.value, false).then(() => renderTasks());
+    createTasks(inputTask.value, false, dayTimer.value, hourTimer.value, minuteTimer.value, 0, true).then(() => renderTasks());
   } 
   inputTask.value = '';
+  dayTimer.value = 0;
+  hourTimer.value = 0;
+  minuteTimer.value = 0;
 })
 
-async function createTasks(title, completed,day, hour, minute) {
+async function createTasks(title, completed, day, hour, minute, second,statusTimer) {
   const task = {
     title,
     completed,
     timer: {
       day,
       hour,
-      minute
+      minute,
+      second,
+      statusTimer
     }
   }
   return fetch(BASE_URL, {
@@ -48,28 +84,63 @@ async function deleteTask(id) {
   })
 }
 
-async function editTasks(id,title, completed) {
+async function editTimerTasks(id, day, hour, minute, second, statusTimer) {
   const task = {
-    title,
-    completed
+    timer: {
+      day,
+      hour,
+      minute,
+      second,
+      statusTimer
+    } 
   }
   return fetch(`${BASE_URL}${id}`, {
-    method: "PUT",
+    method: "PATCH",
+    body: JSON.stringify(task)
+  })
+}
+
+async function editTasks(id, title) {
+  const task = {
+    title
+     
+  }
+
+  return fetch(`${BASE_URL}${id}`, {
+    method: "PATCH",
+    body: JSON.stringify(task)
+  })
+}
+
+async function editTasksCompleted(id, title, completed) {
+  const task = {
+    title,
+    completed,
+    } 
+
+  return fetch(`${BASE_URL}${id}`, {
+    method: "PATCH",
     body: JSON.stringify(task)
   })
 }
 
 async function renderTasks() {
-  listTasks.textContent = '';
-  const response = await fetch(BASE_URL);
+  const response =  await fetch(BASE_URL);
   const tasks = await response.json();
-
-  tasks.forEach((task) => {
-    console.log(task.title);
+  listTasks.textContent = '';
+  
+  tasks.forEach((task) => { 
     const newString = document.createElement('div');
     const newTask = document.createElement('div');
     const newDelete = document.createElement('div');
     const newEdit = document.createElement('div');
+    const newTimer = document.createElement('div');
+    const newDay = document.createElement('div');
+    const newHour = document.createElement('div');
+    const newMinute = document.createElement('div');
+    const newSecond = document.createElement('div');
+    const newStart = document.createElement('div');
+    const newPause = document.createElement('div');
     newTask.textContent = task.title;
     newString.className = 'string';
     newTask.className = 'task';
@@ -78,20 +149,37 @@ async function renderTasks() {
     if (!!task.completed) {
       newTask.classList.add('task-do');
       listTasks.append(newString); 
+      newString.prepend(newTask, newEdit,newDelete);
     } else {
-      listTasks.prepend(newString)
+      listTasks.prepend(newString);
+      newDay.textContent = task.timer.day;
+      newHour.textContent = task.timer.hour;
+      newMinute.textContent = task.timer.minute;
+      newSecond.textContent = task.timer.second;
+      newTimer.className = 'timer';
+      newDay.className ='timer-item day-timer';
+      newHour.className ='timer-item hour-timer';
+      newMinute.className ='timer-item minute-timer';
+      newSecond.className ='timer-item second-timer';
+      newStart.className = 'timer-start';
+      newPause.classList = 'timer-pause';
+      newTimer.prepend(newDay,newHour,newMinute, newSecond, newPause, newStart);
+      newString.prepend(newTask,newTimer,newEdit,newDelete);
+    
+    if(!!task.timer.statusTimer) {
+      let newTimerStart = timerStart(newDay, newHour, newMinute, newSecond, task.timer.day, task.timer.hour, task.timer.minute, task.timer.second);
+      newTimerStart();
     }
-    newString.prepend(newTask,newEdit,newDelete);
 
-    const newTimer = document.createElement('div');
-    const newDay = document.createElement('div');
-    const newHour = document.createElement('div');
-    const newMinute= document.createElement('div');
-    newDay.textContent = task.timer.day;
-    newHour.textContent = task.timer.hour;
-    newMinute.textContent = task.timer.minute;
-    newString.prepend(newTimer);
-    newTimer.prepend(newDay,newHour,newMinute)
+    newPause.addEventListener('click', () => {
+      editTimerTasks(task.id, newDay.textContent, newHour.textContent, newMinute.textContent, newSecond.textContent).then(() => renderTasks());
+    })
+
+    newStart.addEventListener('click', () => {
+      let newTimerStart = timerStart(newDay, newHour, newMinute, newSecond, task.timer.day, task.timer.hour, task.timer.minute, task.timer.second);
+      newTimerStart();
+    })
+  }
 
     newDelete.addEventListener('click', () => {
       deleteTask(task.id).then(() => renderTasks());
@@ -99,20 +187,19 @@ async function renderTasks() {
 
     newTask.addEventListener('click', () => {
       if (!task.completed) {
-        editTasks(task.id,task.title,true).then(() => renderTasks());
+        editTasksCompleted(task.id, task.title, true).then(() => renderTasks());
       } 
       else {
-        editTasks(task.id,task.title,false).then(() => renderTasks());
+        editTasksCompleted(task.id, task.title, false).then(() => renderTasks());
       }
     })
 
     newEdit.addEventListener('click', () => {
       taskId = task.id;
-      taskCompleted = task.completed;
       inputTask.value = task.title;
       buttonForm.textContent = "Редактировать";
-
     })
+
   });
 }
 
